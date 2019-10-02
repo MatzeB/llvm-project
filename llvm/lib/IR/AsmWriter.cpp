8e9c37e14c582a4f4c3efb861940134fc8442842
Wenlei He <wenlei@fb.com>
2019-10-02 15:39:47 -0700

    Rebase: [print-for-dev] Add function start line
    Summary:
    Add function start line to the right of function signature. This makes calculating line offset easier, which is used as anchor for AutoFDO profile. To accommodate this extra info, I moved entry/total sample count above function signature.
    
    Before:
    
    ```
    ; Function Attrs: noinline nounwind optnone uwtable
    define dso_local i32 @foo(i32 %0) #0 function_entry_count 101 function_total_samples 10000 { [ tiny.c:1 ]
      successors: %3
    ```
    
    After:
    ```
    ; Function Attrs: noinline nounwind optnone uwtable
    ; Function Profile: function_entry_count(101), function_total_samples(10000)
    define dso_local i32 @foo(i32 %0) #0 {                                          [ tiny.c:1 ]
      successors: %3
    ```
    
    Manual rebase conflict history:
    https://phabricator.intern.facebook.com/D17726872
    
    Test Plan: verified -print-for-dev output; ninja check
    
    Reviewers: #compiler_toolchain
    
    Differential Revision: https://phabricator.intern.facebook.com/D20111437
    
    Tasks: T44360418

--- llvm/lib/IR/AsmWriter.cpp
+++ llvm/lib/IR/AsmWriter.cpp
@@ -2665,6 +2665,7 @@ public:
 
   // facebook begin T44538829
   void printNeighborBlocksWithWeights(const BasicBlock *BB);
+  void printFunctionDefWithProfile(const Function *F);
   // facebook end
 
 private:
@@ -3774,8 +3775,18 @@ void AssemblyWriter::printFunction(const Function *F) {
     F->getAllMetadata(MDs);
     printMetadataAttachments(MDs, " ");
     Out << ' ';
-  } else
+  } else {
+    // facebook begin T44360418
+    // Under PrintForDev mode, print metadata related to the profile counter.
+    // The profile counter attached to the instructions won't even printed,
+    // because printInstruction function returns before the metadata printing
+    // under PrintForDev.
+    if (PrintForDev) {
+      printFunctionDefWithProfile(F);
+    }
+    // facebook end
     Out << "define ";
+  }
 
   Out << getLinkageNameWithSpace(F->getLinkage());
   PrintDSOLocation(*F, Out);
@@ -3872,11 +3883,21 @@ void AssemblyWriter::printFunction(const Function *F) {
   if (F->isDeclaration()) {
     Out << '\n';
   } else {
-    SmallVector<std::pair<unsigned, MDNode *>, 4> MDs;
-    F->getAllMetadata(MDs);
-    printMetadataAttachments(MDs, " ");
+    // facebook begin T44538829
+    if (IsForDev) {
+      Out << " {";
+      if (DISubprogram *SP = F->getSubprogram()) {
+        Out.PadToColumn(LocationColumn);
+        Out << "[ " << SP->getFilename() << ":" << SP->getLine() << " ]";
+      }
+    } else {
+      SmallVector<std::pair<unsigned, MDNode *>, 4> MDs;
+      F->getAllMetadata(MDs);
+      printMetadataAttachments(MDs, " ");
+      Out << " {";
+    }
+    // facebook end
 
-    Out << " {";
     // Output all of the function's basic blocks.
     for (const BasicBlock &BB : *F)
       printBasicBlock(&BB);
@@ -3956,6 +3977,24 @@ void AssemblyWriter::printNeighborBlocksWithWeights(const BasicBlock *BB) {
     }
     ++succIndex;
   }
+}
+
+void AssemblyWriter::printFunctionDefWithProfile(const Function *F) {
+  SmallVector<std::pair<unsigned, MDNode *>, 4> MDs;
+  F->getAllMetadata(MDs);
+  bool FirstMeta = true;
+  for (const auto &I : MDs) {
+    unsigned Kind = I.first;
+    if (Kind == LLVMContext::MD_prof) {
+      auto *Name = dyn_cast<MDString>(I.second->getOperand(0));
+      ConstantInt *CI = mdconst::extract<ConstantInt>(I.second->getOperand(1));
+      Out << (FirstMeta ? "; Function Profile: " : ", ") << Name->getString()
+          << "(" << CI->getZExtValue() << ")";
+      FirstMeta = false;
+    }
+  }
+  if (!FirstMeta)
+    Out << "\n";
 }
 // facebook end
 
@@ -4520,24 +4559,8 @@ void AssemblyWriter::printMetadataAttachments(
     MDs[0].second->getContext().getMDKindNames(MDNames);
 
   // facebook begin T44360418
-  // Under PrintForDev mode, only print metadata related to the profile counter.
-  // The profile counter attached to the instructions won't even printed,
-  // because printInstruction function returns before the metadata printing
-  // under PrintForDev.
-  if (PrintForDev) {
-    for (const auto &I : MDs) {
-      unsigned Kind = I.first;
-      if (Kind == LLVMContext::MD_prof) {
-        Out << Separator;
-        auto *Name = dyn_cast<MDString>(I.second->getOperand(0));
-        Out << Name->getString() << " ";
-        ConstantInt *CI =
-            mdconst::extract<ConstantInt>(I.second->getOperand(1));
-        Out << CI->getZExtValue();
-      }
-    }
+  if (PrintForDev)
     return;
-  }
   // facebook end
 
   auto WriterCtx = getContext();
