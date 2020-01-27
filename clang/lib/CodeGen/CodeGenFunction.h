4c57286ce32852c0ec50633edacdbdc5f621cdb5
Wenlei He <wenlei@meta.com>
2020-01-27 09:06:27 -0800

    Rebase: Disable optimizations for functions with large number of constructor calls.
    Summary:
    Functions with many statements or conditional branches may cause performance issues to the optimizer. An instance is the static initializer MegaChildToMegaParentsMap in fbcode which has 10000+ elements in an array initialization and causes stack overflow to an optimizer pass (ScalarEvolution). I'm making a change to give up optimizations and lifetime marker annotation to such functions for compiler throughput and memory efficiency, based on the assumption that such functions are usually machine-generated and less performance-concerned. The change could also benefit a smaller code size, given that fewer lifetime markers will be generated.
    
    This change does not fully fix all static initializer situations. Particularly it fixes the following scenario
    ```
    std::map<int, list<int>> const& GetHashTable()
    {
        static map<int, list<int>> UnderlyingHashTable (
        {
            {888, std::initializer_list<int>{100, 191}},
            {266, std::initializer_list<int>{80, 81}},
            // 10000+ entries
            ....
            }
       );
        return UnderlyingHashTable;
    }
    ```
    
    A new switch "-mllvm -opt-cxxconstruct-limit=num" can be used to control the threshold.
    
    Without this change in optimization mode, the front end generates a lifetime marker and unwind path for each of the element (of type std::pair<int, list<int>> construction since a std::pair construction may throw an exception. The unwind path will be a piece of consecutive blocks destroying previous constructed pairs up to an exception throwing point. The consecutive blocks come from lifetime markers for each single pair object. When optimizations are disabled, no lifetime markers are emitted and the consecutive unwind blocks are transformed to a single loop instead. This is why I'd like to call out a potential size win.
    
    Manual rebase conflict history:
    https://phabricator.intern.facebook.com/D19581768
    https://phabricator.intern.facebook.com/D20330001
    https://phabricator.intern.facebook.com/D22019322
    https://phabricator.intern.facebook.com/D22157290
    https://phabricator.intern.facebook.com/D24363737
    https://phabricator.intern.facebook.com/D24394060
    https://phabricator.intern.facebook.com/D24545747
    https://phabricator.intern.facebook.com/D37473583
    https://phabricator.intern.facebook.com/D46764343
    https://phabricator.intern.facebook.com/D47309193
    
    Test Plan:
    check-llvm
    check-clang
    fbcode adreviewer successfully builds
    I'll need to collect size data to show if there is any size wins
    
    Reviewers: #compiler_toolchain
    
    Subscribers: wenlei, phabricatorlinter, #compiler_toolchain
    
    Differential Revision: https://phabricator.intern.facebook.com/D58788840
    
    Tasks: T60099708
    
    Tags: bootcamp

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -2128,7 +2128,8 @@ private:
   void EmitKernelMetadata(const FunctionDecl *FD, llvm::Function *Fn);
 
 public:
-  CodeGenFunction(CodeGenModule &cgm, bool suppressNewContext=false);
+  CodeGenFunction(CodeGenModule &cgm, bool suppressNewContext = false,
+                  llvm::Function *Fn = nullptr);
   ~CodeGenFunction();
 
   CodeGenTypes &getTypes() const { return CGM.getTypes(); }
