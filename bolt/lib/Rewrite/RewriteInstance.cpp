c4c84372f4632142ef57b3b34bd5ba1a3a8063d8
Rafael Auler <rafaelauler@fb.com>
2022-11-09 17:45:40 -0800

    [HOTFIX] Revert "[BOLT] Disable -lite when split function is present"
    This reverts commit 153eeb4a5e68c6054a220667646b14f3785a942e.

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -1453,17 +1453,10 @@ void RewriteInstance::adjustFunctionBoundaries() {
         Function.hasRestoredNameRegex(".*\\.cold(\\.[0-9]+)?");
     if (FragName) {
       static bool PrintedWarning = false;
-      if (!PrintedWarning) {
-        PrintedWarning = true;
+      if (BC->HasRelocations && !PrintedWarning) {
         errs() << "BOLT-WARNING: split function detected on input : "
-               << *FragName;
-        if (BC->HasRelocations)
-          errs() << ". The support is limited in relocation mode";
-        if (opts::Lite) {
-          opts::Lite = false;
-          errs() << "\nBOLT-WARNING: disabling lite mode (-lite) when split "
-                 << "functions are present\n";
-        }
+               << *FragName << ". The support is limited in relocation mode.\n";
+        PrintedWarning = true;
       }
       Function.IsFragment = true;
     }
