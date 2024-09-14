a406eb494517e3bf9fd9f2f8b29354f640e0645f
Rafael Auler <rafaelauler@meta.com>
2024-09-14 11:13:44 -0700

    Rebase: [BOLT][AArch64] Add hacky interface for adding thunk functions
    Summary:
    Not for upstream. Primitive interface for thunks.
    
    Manual rebase conflict history:
    https://phabricator.intern.facebook.com/D62690168
    
    Test Plan: sandcastle
    
    Reviewers: #llvm-bolt
    
    Differential Revision: https://phabricator.intern.facebook.com/D70532005
    
    Tags: aarch64

--- bolt/include/bolt/Core/BinaryContext.h
+++ bolt/include/bolt/Core/BinaryContext.h
@@ -231,6 +231,12 @@ class BinaryContext {
   /// Functions injected by BOLT
   std::vector<BinaryFunction *> InjectedBinaryFunctions;
 
+  /// Thunk functions.
+  std::vector<BinaryFunction *> ThunkBinaryFunctions;
+
+  /// Function that precedes thunks in the binary.
+  const BinaryFunction *ThunkLocation{nullptr};
+
   /// Jump tables for all functions mapped by address.
   std::map<uint64_t, JumpTable *> JumpTables;
 
@@ -567,6 +573,16 @@ public:
   std::vector<BinaryFunction *> &getInjectedBinaryFunctions() {
     return InjectedBinaryFunctions;
   }
+
+  BinaryFunction *createThunkBinaryFunction(const std::string &Name);
+
+  std::vector<BinaryFunction *> &getThunkBinaryFunctions() {
+    return ThunkBinaryFunctions;
+  }
+
+  const BinaryFunction *getThunkLocation() const { return ThunkLocation; }
+
+  void setThunkLocation(const BinaryFunction *BF) { ThunkLocation = BF; }
 
   /// Return vector with all functions, i.e. include functions from the input
   /// binary and functions created by BOLT.
