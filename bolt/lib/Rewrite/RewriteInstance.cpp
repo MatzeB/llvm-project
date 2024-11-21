49ad312ec77950c76ffe5e3561b42099c4875216
Maksim Panchenko <maks@fb.com>
2024-11-20 22:44:09 -0800

    [BOLT][WIP] Skip creation of new segments
    Summary:
    When new contents are added to the new segment, do not modify program
    header table and don't add an empty segment that can still eat some space.
    
    Currently, this feature works only with --use-gnu-stack. I want to
    extend the functionality before sending the patch upstream, but it works
    internally.
    
    Test Plan: SC
    
    Reviewers: ayermolo, rafaelauler, #llvm-bolt
    
    Reviewed By: ayermolo
    
    Subscribers: aaupov
    
    Differential Revision: https://phabricator.intern.facebook.com/D66310046

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -4067,6 +4067,11 @@ void RewriteInstance::patchELFPHDRTable() {
     NewWritableSegmentSize = NextAvailableAddress - NewWritableSegmentAddress;
   }
 
+  if (!NewTextSegmentSize && !NewWritableSegmentSize) {
+    BC->outs() << "BOLT-INFO: not adding new segments\n";
+    return;
+  }
+
   const uint64_t SavedPos = OS.tell();
   OS.seek(PHDRTableOffset);
 
