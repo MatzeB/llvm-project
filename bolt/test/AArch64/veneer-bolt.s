828e52559cd03b294efc1056253595b1d25eaa82
Maksim Panchenko <maks@meta.com>
2025-06-10 13:48:33 -0700

    [BOLT][AArch64] Add symbols for BOLT thunks
    Summary:
    Since thunks are now emitted as injected functions, their names will appear in the symbol table.
    
    ## Summary
    The diff refactors the BOLT thunks creation and management. Instead of having a separate data structure to keep track of thunk functions, it now reuses the existing infrastructure for injected functions.
    
    The changes include:
    - Removing `ThunkBinaryFunctions` and related code from `BinaryContext`.
    - Changing `createThunkBinaryFunction` to use `createInjectedBinaryFunction` instead.
    - Removing `ThunkLocation` and `getThunkBinaryFunctions` as they are no longer needed.
    
    This refactoring simplifies the codebase and reduces the maintenance burden.
    
    Test Plan: SC
    
    Rollback Plan:
    
    Reviewers: aaupov, #llvm-bolt, shatian
    
    Reviewed By: aaupov
    
    Differential Revision: https://phabricator.intern.facebook.com/D76367861
    
    Tags: aarch64

--- bolt/test/AArch64/veneer-bolt.s
+++ bolt/test/AArch64/veneer-bolt.s
@@ -8,6 +8,7 @@
 # RUN: llvm-bolt %t.exe -o %t.bolt --relax-exp --lite=1 --data %t.fdata
 # RUN: llvm-bolt %t.exe -o %t.bolt --relax-exp --hot-functions-at-end --lite=0 \
 # RUN:   --data %t.fdata
+# RUN: llvm-objdump -d %t.bolt | FileCheck %s
 
 ## The constant island at the end of a function makes it ~112MB in size.
 ## Thus the total code size exceeds 300MB.
@@ -40,6 +41,11 @@ hot:
   bl _start
   ret
   .size hot, .-hot
+
+# CHECK-LABEL: <hot>:
+# CHECK-NEXT: bl {{.*}} <__AArch64ADRPThunk_foo>
+# CHECK-NEXT: bl {{.*}} <__AArch64Thunk_bar>
+# CHECK-NEXT: bl {{.*}} <_start>
 
   .global _start
   .type _start, %function
