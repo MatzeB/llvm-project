3be0118f64e5559f41477866637d87cdc4e2eade
Dmitry Polukhin <34227995+dmpolukhin@users.noreply.github.com>
2025-06-17 09:45:18 +0100

    [server-llvm-19][C++20][Modules] Disable preferred_name when writing a C++20 header unit (#144377)
    Summary:
    Backport of: https://github.com/llvm/llvm-project/pull/144377
    
    https://reviews.llvm.org/D130331 added workaround for named modules
    only. But the same issue happens for headees units. Link issue #56490
    
    Test Plan: check-clang
    
    Rollback Plan:
    
    Reviewers: juanbesa, #compiler_toolchain, wenlei
    
    Reviewed By: juanbesa
    
    Differential Revision: https://phabricator.intern.facebook.com/D76896422
    
    Tags: accept2ship

--- clang/include/clang/Serialization/ASTWriter.h
+++ clang/include/clang/Serialization/ASTWriter.h
@@ -860,6 +860,10 @@ public:
     return WritingModule && WritingModule->isNamedModule();
   }
 
+  bool isWritingStdCXXHeaderUnit() const {
+    return WritingModule && WritingModule->isHeaderUnit();
+  }
+
   bool isGeneratingReducedBMI() const { return GeneratingReducedBMI; }
 
   bool getDoneWritingDeclsAndTypes() const { return DoneWritingDeclsAndTypes; }
