10d08eccad1431df19f475463d213b7441b69c87
Maksim Panchenko <maks@fb.com>
2025-06-25 21:11:48 -0700

    [BOLT] Refactor getOutputFunctions() interface
    Summary:
    Change getOutputFunctions() interface to return a copy of the output
    function list. updateOutputFunctions() should be used for updating the
    list.
    
    Update OutputFunctions list whenever a newly injected function is
    created. This ensures that getOutputFunctions() is in sync with new
    functions.
    
    Test Plan: test case updated; build dev mode HHVM
    
    Rollback Plan:
    
    Reviewers: aaupov, #llvm-bolt
    
    Reviewed By: aaupov
    
    Differential Revision: https://phabricator.intern.facebook.com/D77350697
    
    Tasks: T228715364

--- bolt/lib/Core/BinaryContext.cpp
+++ bolt/lib/Core/BinaryContext.cpp
@@ -1589,7 +1589,7 @@ unsigned BinaryContext::addDebugFilenameToUnit(const uint32_t DestCUID,
                                DestCUID, DstUnit->getVersion()));
 }
 
-std::vector<BinaryFunction *> &BinaryContext::getOutputFunctions() {
+const std::vector<BinaryFunction *> &BinaryContext::getOutputFunctions() {
   assert((!HasRelocations || HasFinalizedFunctionOrder) &&
          "Output function order not finalized");
 
@@ -1624,6 +1624,11 @@ std::vector<BinaryFunction *> &BinaryContext::getOutputFunctions() {
   llvm::copy(InjectedBinaryFunctions, std::back_inserter(OutputFunctions));
 
   return OutputFunctions;
+}
+
+void BinaryContext::updateOutputFunctions(
+    std::vector<BinaryFunction *> &Functions) {
+  OutputFunctions.swap(Functions);
 }
 
 std::vector<BinaryFunction *> BinaryContext::getAllBinaryFunctions() {
@@ -2455,6 +2460,10 @@ BinaryContext::createInjectedBinaryFunction(const std::string &Name,
   BinaryFunction *BF = InjectedBinaryFunctions.back();
   setSymbolToFunctionMap(BF->getSymbol(), BF);
   BF->CurrentState = BinaryFunction::State::CFG;
+
+  if (!OutputFunctions.empty())
+    OutputFunctions.push_back(BF);
+
   return BF;
 }
 
