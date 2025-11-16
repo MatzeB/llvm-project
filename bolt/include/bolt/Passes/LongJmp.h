4a76ab4040c3e57191784917c577285350254e47
Maksim Panchenko <maks@meta.com>
2025-11-15 23:58:11 -0800

    [BOLT][AArch64] Fix secondary entry point thunks
    Summary:
    Fix an issue with secondary entry point AArch64 thunks.
    
    For thunks generation, we used to track `BinaryFunction` as a target.
    However, it's not sufficient for secondary entry points, as we might
    need more than one thunk per function.
    
    Instead of tracking `BinaryFunction`s, track `MCSymbol`s associated with
    entry points. Special care is applied to tracking multiple symbols per
    main entry point when different symbols are used for calling the same
    function.
    
    Test Plan: SC. `shots_server` to be added to large binary tests.
    
    Rollback Plan:
    
    Reviewers: yongzhu, aaupov, #llvm-bolt
    
    Reviewed By: yongzhu, aaupov
    
    Differential Revision: https://phabricator.intern.facebook.com/D87279370
    
    Tasks: T245360662
    
    Tags: aarch64

--- bolt/include/bolt/Passes/LongJmp.h
+++ bolt/include/bolt/Passes/LongJmp.h
@@ -77,27 +77,29 @@ class LongJmpPass : public BinaryFunctionPass {
   void relaxLocalBranches(BinaryFunction &BF);
 
   struct FunctionCluster {
-    // All functions in this cluster.
+    /// All functions in this cluster.
     DenseSet<BinaryFunction *> Functions;
 
-    // Functions that this cluster of functions is calling. Note that it
-    // excludes all functions in the cluster itself.
-    DenseSet<BinaryFunction *> Callees;
+    /// Symbols corresponding to entry points of functions that this cluster
+    /// calls. Note that it excludes all functions in the cluster itself.
+    DenseSet<const MCSymbol *> Callees;
 
-    // Estimated size of the cluster in bytes.
+    /// Estimated size of the cluster in bytes.
     uint64_t Size{0};
 
-    // The index of the last function in the cluster. Used as an insertion point
-    // for adding thunks to the output function list.
+    /// The index of the last function in the cluster. Used as an insertion
+    /// point for adding thunks to the output function list.
     size_t FirstFunctionIndex = -1;
     size_t LastFunctionIndex = -1;
 
-    // Thunks located at the end of this cluster.
+    /// Thunks located at the end of this cluster.
     std::vector<BinaryFunction *> ThunkList;
 
-    // Thunks used by this cluster. Some could be in a ThunkList of the
-    // preceding cluster.
-    DenseMap<BinaryFunction *, BinaryFunction *> Thunks;
+    /// Thunks used by this cluster. Some could be in a ThunkList of the
+    /// preceding cluster.
+    ///
+    /// <Function Symbol> -> <Thunk Function>.
+    DenseMap<const MCSymbol *, BinaryFunction *> Thunks;
   };
 
   /// Maximum size of the function cluster. Note that it's less than 128MB
