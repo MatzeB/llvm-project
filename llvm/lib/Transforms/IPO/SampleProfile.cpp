ad7eb309b9e5c9817faee57c9cdf74e3ce24c806
HighW4y2H3ll <zhenghaohuu@gmail.com>
2026-03-12 11:53:57 -1000

    [server-llvm-19][SampleProfile] Skip counting mismatched weak symbols during profile loading (#185514)
    Summary:
    Backport: https://github.com/llvm/llvm-project/commit/e61540086130684d8de66d883f693bf4da2ad5b7
    
    Weak symbols may be overridden during linking, and this may cause
    profile mismatch when compiling the weak symbols, while the profile was
    created based on the overriding function. Skip counting the weak symbol
    while checking the mismatched function profiles to avoid false alarm on
    rejecting legit profiles.
    
    Test Plan: CI
    
    Reviewers: aaupov, #compiler_toolchain
    
    Reviewed By: aaupov
    
    Differential Revision: https://phabricator.intern.facebook.com/D96385591

--- llvm/lib/Transforms/IPO/SampleProfile.cpp
+++ llvm/lib/Transforms/IPO/SampleProfile.cpp
@@ -2120,7 +2120,8 @@ bool SampleProfileLoader::rejectHighStalenessProfile(
       continue;
 
     TotalHotFunc++;
-    if (ProbeManager->profileIsHashMismatched(*FuncDesc, FS))
+    if (ProbeManager->profileIsHashMismatched(*FuncDesc, FS) &&
+        !ProbeManager->probeFromWeakSymbol(FS.getGUID()))
       NumMismatchedFunc++;
   }
   // Make sure that the num of selected function is not too small to distinguish
